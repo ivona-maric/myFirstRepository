7715dc889d94c4ccd1455bf27f579b82f1a2aed1
MAOP <dani.kovacic@maop.si>
2019-10-22 23:09:41 +0200

    FirstSpringBootApplication generated in Spring Initializer

---
+++ src/test/java/si/maop/test/firstspringboot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package si.maop.test.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
